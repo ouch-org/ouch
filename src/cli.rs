d2af261f67210073b059e6a891e1afcda6a79ff3
Vinícius Rodrigues Miguel <lemao.vrm07@hotmail.com>
2021-04-05 01:14:27 -0300

    Fix `cargo test` tests

--- src/cli.rs
+++ src/cli.rs
@@ -66,8 +66,9 @@ pub fn parse_args_from(mut args: Vec<OsString>) -> crate::Result<ParsedArgs> {
             let mut files: Vec<PathBuf> = args.into_iter().map(PathBuf::from).collect();
 
             if files.len() < 2 {
-                panic!("The compress subcommands demands at least 2 arguments, see usage:.......");
+                return Err(crate::Error::MissingArgumentsForCompression);
             }
+
             // Safety: we checked that args.len() >= 2
             let compressed_output_path = files.pop().unwrap();
 
@@ -102,98 +103,4 @@ pub fn parse_args_from(mut args: Vec<OsString>) -> crate::Result<ParsedArgs> {
     };
 
     Ok(parsed_args)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    fn gen_args(text: &str) -> Vec<OsString> {
-        let args = text.split_whitespace();
-        args.map(OsString::from).collect()
-    }
-
-    // // util for test the argument parsing
-    // macro_rules! test {
-    //     ($expected_command:expr, $input_text:expr) => {{
-    //         assert_eq!(
-    //             $expected_command,
-    //             oof::try_arg_parsing($input_text.split_whitespace())
-    //         )
-    //     }};
-    // }
-
-    macro_rules! parse {
-        ($input_text:expr) => {{
-            let args = gen_args($input_text);
-            parse_args_from(args).unwrap()
-        }};
-    }
-
-    #[test]
-    // The absolute flags that ignore all the other argparsing rules are --help and --version
-    fn test_absolute_flags() {
-        let expected = Command::ShowHelp;
-        assert_eq!(expected, parse!("").command);
-        assert_eq!(expected, parse!("-h").command);
-        assert_eq!(expected, parse!("--help").command);
-        assert_eq!(expected, parse!("aaaaaaaa --help -o -e aaa").command);
-        assert_eq!(expected, parse!("aaaaaaaa -h").command);
-        assert_eq!(expected, parse!("--help compress aaaaaaaa").command);
-        assert_eq!(expected, parse!("compress --help").command);
-        assert_eq!(expected, parse!("--version --help").command);
-        assert_eq!(expected, parse!("aaaaaaaa -v aaaa -h").command);
-
-        let expected = Command::ShowVersion;
-        assert_eq!(expected, parse!("ouch --version").command);
-        assert_eq!(expected, parse!("ouch a --version b").command);
-    }
-
-    #[test]
-    fn test_arg_parsing_compress_subcommand() {
-        let files = ["a", "b", "c"].iter().map(PathBuf::from).collect();
-
-        let expected = Command::Compress {
-            files,
-            compressed_output_path: "d".into(),
-        };
-        assert_eq!(expected, parse!("compress a b c d").command);
-    }
-
-    #[test]
-    fn test_arg_parsing_decompress_subcommand() {
-        let files: Vec<_> = ["a", "b", "c"].iter().map(PathBuf::from).collect();
-
-        let expected = Command::Decompress {
-            files: files.clone(),
-            output_folder: None,
-        };
-        assert_eq!(expected, parse!("a b c").command);
-
-        let expected = Command::Decompress {
-            files,
-            output_folder: Some("folder".into()),
-        };
-        assert_eq!(expected, parse!("a b c --output folder").command);
-        assert_eq!(expected, parse!("a b --output folder c").command);
-        assert_eq!(expected, parse!("a --output folder b c").command);
-        assert_eq!(expected, parse!("--output folder a b c").command);
-    }
-
-    // #[test]
-    // fn test_arg_parsing_decompress_subcommand() {
-    //     let files: Vec<PathBuf> = ["a", "b", "c"].iter().map(PathBuf::from).collect();
-
-    //     let expected = Ok(Command::Decompress {
-    //         files: files.clone(),
-    //     });
-    //     test!(expected, "ouch a b c");
-
-    //     let files: Vec<PathBuf> = ["a", "b", "c", "d"].iter().map(PathBuf::from).collect();
-
-    //     let expected = Ok(Command::Decompress {
-    //         files: files.clone(),
-    //     });
-    //     test!(expected, "ouch a b c d");
-    // }
 }
