739a52f00e3ecfc241ac0c7049cd95aaa0521cd7
Vinícius Miguel <36349314+vrmiguel@users.noreply.github.com>
2021-11-05 22:04:00 -0400

    Refactor utils into a module (#166)

---
+++ src/utils/mod.rs
@@ -0,0 +1,12 @@
+//! Random filesystem-related stuff used on ouch.
+
+mod bytes;
+mod fs;
+mod question_policy;
+
+pub use bytes::Bytes;
+pub use fs::{
+    cd_into_same_dir_as, colors, concatenate_list_of_os_str, create_dir_if_non_existent, dir_is_empty,
+    nice_directory_display, strip_cur_dir, to_utf,
+};
+pub use question_policy::{create_or_ask_overwrite, user_wants_to_overwrite, QuestionPolicy};
