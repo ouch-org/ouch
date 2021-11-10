a531d44e2b4add69f4a7bc505310994051de61d7
João M. Bezerra <marcospb19@hotmail.com>
2021-11-10 09:55:31 -0300

    Move colors module to it's own file

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,11 +1,12 @@
 //! Random and miscellaneous utils used in ouch.
 
+pub mod colors;
 mod formatting;
 mod fs;
 mod question;
 
 pub use formatting::{concatenate_os_str_list, nice_directory_display, strip_cur_dir, to_utf, Bytes};
-pub use fs::{cd_into_same_dir_as, colors, create_dir_if_non_existent, dir_is_empty, try_infer_extension};
+pub use fs::{cd_into_same_dir_as, create_dir_if_non_existent, dir_is_empty, try_infer_extension};
 pub use question::{
     create_or_ask_overwrite, user_wants_to_continue_decompressing, user_wants_to_overwrite, QuestionPolicy,
 };
