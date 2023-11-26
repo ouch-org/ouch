8b8ab42f0c6bcf9b139a4dd82cf5aec9316fb549
Flat <flat@esoteric.moe>
2023-11-26 17:04:03 -0500

    ci: fix release script

--- scripts/package-release-assets.sh
+++ scripts/package-release-assets.sh
@@ -9,14 +9,19 @@ for dir in ouch-*; do
     cp -r "$dir/artifacts" "$dir/completions"
     mkdir "$dir/man"
     mv "$dir"/completions/*.1 "$dir/man"
+
     cp ../{README.md,LICENSE,CHANGELOG.md} "$dir"
     rm -r "$dir/artifacts"
 
     if [[ "$dir" = *.exe ]]; then
         target=${dir%.exe}
+        mv "$dir/target/${target/ouch-/}/release/ouch.exe" "$dir"
+        rm -r "$dir/target"
         mv "$dir" "$target"
         zip -r "../release/$target.zip" "$target"
     else
+        mv "$dir/target/${dir/ouch-/}/release/ouch" "$dir"
+        rm -r "$dir/target"
         chmod +x "$dir/ouch"
         tar czf "../release/$dir.tar.gz" "$dir"
     fi
