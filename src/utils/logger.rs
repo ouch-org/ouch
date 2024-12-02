8e595f153a6dd2b4a1ec45ebae0d063c621673ca
ttyS3 <ttys3.rust@gmail.com>
2024-12-02 12:50:46 +0000

    refactor: improve code formatting in `mod.rs` and `logger.rs`

--- src/utils/logger.rs
+++ src/utils/logger.rs
@@ -1,5 +1,7 @@
-use std::sync::{mpsc, Arc, Barrier, OnceLock};
-use std::thread;
+use std::{
+    sync::{mpsc, Arc, Barrier, OnceLock},
+    thread,
+};
 
 pub use logger_thread::spawn_logger_thread;
 
