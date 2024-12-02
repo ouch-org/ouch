77b01d170fdecf933c236625b3552c21c0c1e45a
ttyS3 <ttys3.rust@gmail.com>
2024-12-02 12:49:19 +0000

    refactor(cli): move thread pool setup to command execution, use thread::spawn instead of rayon::spawn in the logger thread

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -53,6 +53,14 @@ pub fn run(
     question_policy: QuestionPolicy,
     file_visibility_policy: FileVisibilityPolicy,
 ) -> crate::Result<()> {
+
+    if let Some(threads) = args.threads {
+        rayon::ThreadPoolBuilder::new()
+            .num_threads(threads)
+            .build_global()
+            .unwrap();
+    }
+
     match args.cmd {
         Subcommand::Compress {
             files,
