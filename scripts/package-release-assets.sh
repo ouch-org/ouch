28820e8a2d1cbe0b8fa4039748b8916aa520cf6c
João Marcos <marcospb19@hotmail.com>
2025-04-06 19:46:00 -0300

    PQ ESSE COMMIT TA FAZENDO AS COISAS AO CONTRARIO, DESISTIR E FAZER TUDO DO ZERO

--- scripts/package-release-assets.sh
+++ scripts/package-release-assets.sh
@@ -1,28 +1,34 @@
 #!/usr/bin/env bash
 set -e
 
+mkdir release
 cd downloaded_artifacts
-mkdir ../assets
 
-for input_dir in ouch-*; do
-    cp ../{README.md,LICENSE,CHANGELOG.md} "$input_dir"
-    mkdir "$input_dir/man"
-    mkdir "$input_dir/artifacts"
+for dir in ouch-*; do
+    mkdir "$dir/man"
+    mkdir "$dir/completions"
+    mv "$dir/artifacts/*.1" "$dir/man"
+    mv "$dir/artifacts/*" "$dir/completions"
+    rm -r "$dir/artifacts"
 
-    mv "$input_dir"/artifacts/*.1 "$input_dir/man"
-    mv "$input_dir"/artifacts/* "$input_dir/completions"
-    rm -r "$input_dir/artifacts"
+    cp ../{README.md,LICENSE,CHANGELOG.md} "$dir"
+    rm -r "$dir/artifacts"
 
-    if [[ "$input_dir" = *.exe ]]; then
-        target=${input_dir%.exe}
-        mv "$input_dir/target/${target/ouch-/}/release/ouch.exe" "$input_dir"
-        rm -r "$input_dir/target"
-        mv "$input_dir" "$target"
-        zip -r "../assets/$target.zip" "$target"
+    if [[ "$dir" == *-pc-windows-* ]]; then
+        binary_path="$dir/target/${target/ouch-/}/release/ouch.exe"
+        rm -r "$dir/target"
+        mv "$dir" "$target"
     else
-        mv "$input_dir/target/${input_dir/ouch-/}/release/ouch" "$input_dir"
-        rm -r "$input_dir/target"
-        chmod +x "$input_dir/ouch"
-        tar czf "../assets/$input_dir.tar.gz" "$input_dir"
+        binary_path="$dir/target/${dir/ouch-/}/release/ouch"
+        rm -r "$dir/target"
+        chmod +x "$dir/ouch"
+        tar czf "../release/$dir.tar.gz" "$dir"
+    fi
+
+    if [[ "$dir" == *-pc-windows-* ]]; then
+        zip -r "../release/$dir.zip" "$dir"
+    else
+        chmod +x "$binary_path"
+        tar czf "../release/$dir.tar.gz" "$dir"
     fi
 done
