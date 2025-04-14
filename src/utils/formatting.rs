0cd8485028816dcfa928bacea1b04a8ec7e20019
Talison Fabio <54823205+talis-fb@users.noreply.github.com>
2025-04-14 13:20:30 -0300

    cargo fmt

--- src/utils/formatting.rs
+++ src/utils/formatting.rs
@@ -110,25 +110,16 @@ impl std::fmt::Display for Bytes {
         debug_assert!(num >= 0.0);
         if num < 1_f64 {
             let num_pretty = format!("{:.2}", num);
-            return write!(
-                f, 
-                "{:>6}   B",
-                num_pretty
-            );
+            return write!(f, "{:>6}   B", num_pretty);
         }
 
         let delimiter = 1000_f64;
         let exponent = cmp::min((num.ln() / 6.90775).floor() as i32, 4);
 
         let num_pretty = format!("{:.2}", (num / delimiter.powi(exponent)));
-        let unit_pretty = format!("{}B",  Bytes::UNIT_PREFIXES[exponent as usize]);
+        let unit_pretty = format!("{}B", Bytes::UNIT_PREFIXES[exponent as usize]);
 
-        write!(
-            f,
-            "{:>6} {:>3}",
-            num_pretty,
-            unit_pretty,
-        )
+        write!(f, "{:>6} {:>3}", num_pretty, unit_pretty,)
     }
 }
 
