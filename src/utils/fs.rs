a85c12a12fbc7a44252a78f76d02e75216c257c9
tommady <tommady@users.noreply.github.com>
2025-07-16 12:32:00 +0000

    add src and dst detection for utils copy_dir
    Signed-off-by: tommady <tommady@users.noreply.github.com>

--- src/utils/fs.rs
+++ src/utils/fs.rs
@@ -212,6 +212,12 @@ pub fn try_infer_extension(path: &Path) -> Option<Extension> {
 
 /// Copy the src directory into the dst directory recursively
 pub fn copy_dir(src: &Path, dst: &Path) -> crate::Result<()> {
+    if !src.exists() || !dst.exists() {
+        return Err(crate::Error::NotFound {
+            error_title: "source or destination directory does not exist".to_string(),
+        });
+    }
+
     for entry in fs::read_dir(src)? {
         let entry = entry?;
         let ty = entry.file_type()?;
