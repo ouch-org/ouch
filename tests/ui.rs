7aff049d9c610d319d05e3314eab53a18cfd55f3
João Marcos <marcospb19@hotmail.com>
2026-02-15 20:45:43 -0300

    heck

--- tests/ui.rs
+++ tests/ui.rs
@@ -40,7 +40,7 @@ fn redact_paths(text: &str, dir: &Path) -> String {
     let dir_name = dir.file_name().and_then(OsStr::to_str).unwrap();
 
     // this regex should be good as long as the path does not contain whitespace characters
-    let slashes = "(/|\\(\\)?)";
+    let slashes = r"(/|\\(\\)?)";
     let re = Regex::new(&format!(r"\S*{slashes}{dir_name}{slashes}")).unwrap();
     re.replace_all(text, "<TMP_DIR>/").into()
 }
