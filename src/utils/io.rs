ouch-org/ouch

commit 205114941d11a8d3ecbdf1999c2432919c85994b

Remove crate test feature "allow_piped_choice" (#908)
By replacing the old call of `io::IsTerminal` by `atty` we can now
distinguish scenarios which makes `allow_piped_choice` unnecessary for
our tests

Cargo.toml
@@ -15,6 +15,7 @@ categories = ["command-line-utilities", "compression", "encoding"]
 description = "A command-line utility for easily compressing and decompressing files and directories."
 
 [dependencies]
+atty = "0.2.14"
 brotli = "7.0.0"
 bstr = { version = "1.10.0", default-features = false, features = ["std"] }
 bytesize = "1.3.0"
@@ -78,7 +79,6 @@ test-strategy = "0.4.0"
 default = ["unrar", "use_zlib", "use_zstd_thin", "bzip3"]
 use_zlib = ["flate2/zlib", "gzp/deflate_zlib", "zip/deflate-flate2-zlib"]
 use_zstd_thin = ["zstd/thin"]
-allow_piped_choice = []
 
 # For generating binaries for releases
 [profile.release]

src/utils/question.rs
@@ -5,7 +5,7 @@
 
 use std::{
     borrow::Cow,
-    io::{stdin, BufRead, IsTerminal},
+    io::{self, stdin, BufRead},
     path::Path,
 };
 
@@ -104,7 +104,7 @@ pub fn ask_to_create_file(
 ) -> Result<Option<fs::File>> {
     match fs::OpenOptions::new().write(true).create_new(true).open(path) {
         Ok(w) => Ok(Some(w)),
-        Err(e) if e.kind() == std::io::ErrorKind::AlreadyExists => {
+        Err(e) if e.kind() == io::ErrorKind::AlreadyExists => {
             let action = match question_policy {
                 QuestionPolicy::AlwaysYes => FileConflitOperation::Overwrite,
                 QuestionPolicy::AlwaysNo => FileConflitOperation::Cancel,
@@ -185,8 +185,7 @@ impl<'a, T: Default> ChoicePrompt<'a, T> {
     pub fn ask(mut self) -> crate::Result<T> {
         let message = self.prompt;
 
-        #[cfg(not(feature = "allow_piped_choice"))]
-        if !stdin().is_terminal() {
+        if atty::is(atty::Stream::Stdin) {
             eprintln!("{message}");
             eprintln!("Pass --yes to proceed");
             return Ok(T::default());
@@ -282,8 +281,7 @@ impl<'a> Confirmation<'a> {
             (Some(placeholder), Some(subs)) => Cow::Owned(self.prompt.replace(placeholder, subs)),
         };
 
-        #[cfg(not(feature = "allow_piped_choice"))]
-        if !stdin().is_terminal() {
+        if atty::is(atty::Stream::Stdin) {
             eprintln!("{message}");
             eprintln!("Pass --yes to proceed");
             return Ok(false);

.github/workflows/build-artifacts-and-run-tests.yml
@@ -104,7 +104,7 @@ jobs:
         id: concat-features
         shell: bash
         run: |
-          FEATURES=(allow_piped_choice)
+          FEATURES=()
           if [[ "${{ matrix.feature-unrar }}" == true ]]; then FEATURES+=(unrar); fi
           if [[ "${{ matrix.feature-use-zlib }}" == true ]]; then FEATURES+=(use_zlib); fi
           if [[ "${{ matrix.feature-use-zstd-thin }}" == true ]]; then FEATURES+=(use_zstd_thin); fi

scripts/package-release-assets.sh
@@ -20,8 +20,7 @@ PLATFORMS=(
     "x86_64-unknown-linux-gnu"
     "x86_64-unknown-linux-musl"
 )
-# TODO: remove allow_piped_choice later
-DEFAULT_FEATURES="allow_piped_choice+unrar+use_zlib+use_zstd_thin+bzip3"
+DEFAULT_FEATURES="unrar+use_zlib+use_zstd_thin+bzip3"
 
 for platform in "${PLATFORMS[@]}"; do
     path="ouch-${platform}"

tests/integration.rs
@@ -97,7 +97,6 @@ fn create_random_files(dir: impl Into<PathBuf>, depth: u8, rng: &mut SmallRng) {
 }
 
 /// Create n random files on directory dir
-#[cfg_attr(not(feature = "allow_piped_choice"), allow(dead_code))]
 fn create_n_random_files(n: usize, dir: impl Into<PathBuf>, rng: &mut SmallRng) {
     let dir: &PathBuf = &dir.into();
 
@@ -300,7 +299,6 @@ fn multiple_files_with_conflict_and_choice_to_not_overwrite(
     assert_same_directory(after, after_backup, false);
 }
 
-#[cfg(feature = "allow_piped_choice")]
 #[proptest(cases = 25)]
 fn multiple_files_with_conflict_and_choice_to_rename(
     ext: DirectoryExtension,
@@ -318,7 +316,7 @@ fn multiple_files_with_conflict_and_choice_to_rename(
     fs::create_dir_all(&dest_files_path).unwrap();
     create_n_random_files(5, &dest_files_path, &mut SmallRng::from_entropy());
 
-    let archive = &root_path.join(format!("archive.{}", merge_extensions(&ext, &extra_extensions)));
+    let archive = &root_path.join(format!("archive.{}", merge_extensions(ext, &extra_extensions)));
     ouch!("-A", "c", &src_files_path, archive);
 
     let dest_files_path_renamed = &root_path.join("dest_files_1");
@@ -336,7 +334,6 @@ fn multiple_files_with_conflict_and_choice_to_rename(
     assert_same_directory(src_files_path, dest_files_path_renamed.join("src_files"), false);
 }
 
-#[cfg(feature = "allow_piped_choice")]
 #[proptest(cases = 25)]
 fn multiple_files_with_conflict_and_choice_to_rename_with_already_a_renamed(
     ext: DirectoryExtension,
@@ -358,7 +355,7 @@ fn multiple_files_with_conflict_and_choice_to_rename_with_already_a_renamed(
     fs::create_dir_all(&dest_files_path_1).unwrap();
     create_n_random_files(5, &dest_files_path_1, &mut SmallRng::from_entropy());
 
-    let archive = &root_path.join(format!("archive.{}", merge_extensions(&ext, &extra_extensions)));
+    let archive = &root_path.join(format!("archive.{}", merge_extensions(ext, &extra_extensions)));
     ouch!("-A", "c", &src_files_path, archive);
 
     let dest_files_path_renamed = &root_path.join("dest_files_2");
@@ -668,7 +665,6 @@ fn enable_gitignore_flag_should_work_without_git(
     );
 }
 
-#[cfg(feature = "allow_piped_choice")]
 #[proptest(cases = 25)]
 fn unpack_multiple_sources_into_the_same_destination_with_merge(
     ext: DirectoryExtension,
@@ -677,13 +673,13 @@ fn unpack_multiple_sources_into_the_same_destination_with_merge(
     let temp_dir = tempdir()?;
     let root_path = temp_dir.path();
     let source_path = root_path
-        .join(format!("example_{}", merge_extensions(&ext, &extra_extensions)))
+        .join(format!("example_{}", merge_extensions(ext, &extra_extensions)))
         .join("sub_a")
         .join("sub_b")
         .join("sub_c");
 
     fs::create_dir_all(&source_path)?;
-    let archive = root_path.join(format!("archive.{}", merge_extensions(&ext, &extra_extensions)));
+    let archive = root_path.join(format!("archive.{}", merge_extensions(ext, &extra_extensions)));
     crate::utils::cargo_bin()
         .arg("compress")
         .args([
@@ -697,7 +693,7 @@ fn unpack_multiple_sources_into_the_same_destination_with_merge(
 
     fs::remove_dir_all(&source_path)?;
     fs::create_dir_all(&source_path)?;
-    let archive1 = root_path.join(format!("archive1.{}", merge_extensions(&ext, &extra_extensions)));
+    let archive1 = root_path.join(format!("archive1.{}", merge_extensions(ext, &extra_extensions)));
     crate::utils::cargo_bin()
         .arg("compress")
         .args([
@@ -709,7 +705,7 @@ fn unpack_multiple_sources_into_the_same_destination_with_merge(
         .assert()
         .success();
 
-    let out_path = root_path.join(format!("out_{}", merge_extensions(&ext, &extra_extensions)));
+    let out_path = root_path.join(format!("out_{}", merge_extensions(ext, &extra_extensions)));
     fs::create_dir_all(&out_path)?;
 
     crate::utils::cargo_bin()

commit 24ae56a3f8481721b16f6c08aab849c1ded4891f

Fix user prompts not working
Bug introduced in PR #908

Cargo.toml
@@ -15,7 +15,6 @@ categories = ["command-line-utilities", "compression", "encoding"]
 description = "A command-line utility for easily compressing and decompressing files and directories."
 
 [dependencies]
-atty = "0.2.14"
 brotli = "7.0.0"
 bstr = { version = "1.10.0", default-features = false, features = ["std"] }
 bytesize = "1.3.0"

src/utils/io.rs
@@ -1,4 +1,9 @@
-use std::io::{self, stderr, stdout, StderrLock, StdoutLock, Write};
+use std::{
+    io::{self, stderr, stdout, StderrLock, StdoutLock, Write},
+    os::unix::fs::MetadataExt,
+};
+
+use fs_err as fs;
 
 use crate::utils::logger;
 
@@ -15,5 +20,12 @@ pub fn lock_and_flush_output_stdio() -> io::Result<StdioOutputLocks> {
     Ok((stdout, stderr))
 }
 
+pub fn is_stdin_dev_null() -> io::Result<bool> {
+    let stdin = fs::metadata("/proc/self/fd/0")?;
+    let null = fs::metadata("/dev/null")?;
+    Ok(stdin.dev() == null.dev() && stdin.ino() == null.ino())
+}
+
+/// Workaround for `dyn Read + Seek`
 pub trait ReadSeek: io::Read + io::Seek {}
 impl<T> ReadSeek for T where T: io::Read + io::Seek {}

src/utils/question.rs
@@ -14,7 +14,12 @@ use fs_err as fs;
 use crate::{
     accessible::is_running_in_accessible_mode,
     error::{Error, FinalError, Result},
-    utils::{self, colors, formatting::path_to_str, io::lock_and_flush_output_stdio, strip_cur_dir},
+    utils::{
+        self, colors,
+        formatting::path_to_str,
+        io::{is_stdin_dev_null, lock_and_flush_output_stdio},
+        strip_cur_dir,
+    },
 };
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
@@ -185,9 +190,9 @@ impl<'a, T: Default> ChoicePrompt<'a, T> {
     pub fn ask(mut self) -> crate::Result<T> {
         let message = self.prompt;
 
-        if atty::is(atty::Stream::Stdin) {
+        if is_stdin_dev_null()? {
             eprintln!("{message}");
-            eprintln!("Pass --yes to proceed");
+            eprintln!("Stdin is null, can't read user input (bypass with --yes, but be careful)");
             return Ok(T::default());
         }
 
@@ -281,9 +286,9 @@ impl<'a> Confirmation<'a> {
             (Some(placeholder), Some(subs)) => Cow::Owned(self.prompt.replace(placeholder, subs)),
         };
 
-        if atty::is(atty::Stream::Stdin) {
+        if is_stdin_dev_null()? {
             eprintln!("{message}");
-            eprintln!("Pass --yes to proceed");
+            eprintln!("Stdin is null, can't read user input (bypass with --yes, but be careful)");
             return Ok(false);
         }
 