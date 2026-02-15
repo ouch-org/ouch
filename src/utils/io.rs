094d8cb03f74b7e4f86a79fb216c64ed1616ece2
João Marcos <marcospb19@hotmail.com>
2026-02-15 15:23:22 -0300

    fix

--- src/utils/io.rs
+++ src/utils/io.rs
@@ -1,7 +1,4 @@
-use std::{
-    io::{self, stderr, stdout, StderrLock, StdoutLock, Write},
-    os::unix::fs::MetadataExt,
-};
+use std::io::{self, stderr, stdout, StderrLock, StdoutLock, Write};
 
 use fs_err as fs;
 
@@ -20,10 +17,18 @@ pub fn lock_and_flush_output_stdio() -> io::Result<StdioOutputLocks> {
     Ok((stdout, stderr))
 }
 
+#[cfg(unix)]
 pub fn is_stdin_dev_null() -> io::Result<bool> {
+    use std::os::unix::fs::MetadataExt;
+
     let stdin = fs::metadata("/proc/self/fd/0")?;
     let null = fs::metadata("/dev/null")?;
     Ok(stdin.dev() == null.dev() && stdin.ino() == null.ino())
+}
+
+#[cfg(not(unix))]
+pub fn is_stdin_dev_null() -> io::Result<bool> {
+    Ok(false)
 }
 
 /// Workaround for `dyn Read + Seek`
